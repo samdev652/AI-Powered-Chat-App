e6bc3528bf310dea603e2513d3b4256bf4a458c4
sambaraks652 <sambaraks652@gmail.com>
2025-11-05 22:25:03 +0300

    added various pages to the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return <div>Hello, World!!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}></Route>
+          <Route index element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div>Hello, People!!</div>;
+};
+
+export default HomePage;
